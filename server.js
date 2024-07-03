1742c974971a7f785d27c12e11bb547b8fe4e927
Francis <francisnjaramba2@gmail.com>
2024-07-03 12:24:10 +0300

    initializer

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/products", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use("/products", require("./routes/products"));
+
+app.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`);
+});
